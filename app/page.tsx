cc1469e064c3e20c9ff2166edfded02e830b4df1
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-02-14 17:22:04 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Image from "next/image";
-
-export default function Home() {
-  return <main>HomePage</main>;
+const Home = () => {
+  return <div>Home</div>
 }
+
+export default Home
---
+++ components/Footer.jsx
@@ -0,0 +1,19 @@
+import { CodeIcon, HeartIcon } from "lucide-react"
+
+const Footer = () => {
+  return (
+    <footer className="py-4">
+      <div
+        className="group flex items-center justify-center gap-1 text-neutral-400 dark:text-neutral-600"
+        aria-hidden={true}
+      >
+        <CodeIcon className="h-5 w-5" />
+        <span>with</span>
+        <HeartIcon className="h-4 w-4 group-hover:text-red-500" />
+        <span>in Denmark</span>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
